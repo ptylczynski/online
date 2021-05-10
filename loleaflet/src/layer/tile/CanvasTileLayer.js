edc906be226a3e068eba1023bc65d0bab21ac2a6
Gökay Şatır <gokay.satir@collabora.com>
2021-05-10 14:14:27 +0300

    Introduce CommentSection.ts file.
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: Ie5e157638df043e39bfb8ee042b004fba19113d2

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -1003,6 +1003,11 @@ L.CanvasTileLayer = L.Layer.extend({
 		if (this._docType === 'spreadsheet') {
 			this._painter._addGridSection();
 		}
+
+		if (this._docType === 'text' || this._docType === 'presentation' || this._docType === 'drawing') {
+			app.sectionContainer.addSection(new app.definitions.CommentSection());
+		}
+
 		this._syncTileContainerSize();
 	},
 
